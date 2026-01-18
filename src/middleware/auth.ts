ad2aeac3d99a5324694402bb445fb4b341e37e46
ohowe <ohowe@duck.com>
2026-01-18 10:46:37 -0700

    make cookie expire at the same time the token does

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -11,7 +11,7 @@ export async function authenticate(c: ContextType, username: string) {
   // Token expires in 1 year
   const token = await sign({ username: username, iss: c.env.JWT_ISSUER, aud: c.env.JWT_ISSUER, iat: Math.floor(Date.now() / 1000), exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 365) }, c.env.JWT_SECRET, ALG);
 
-  setCookie(c, AUTH_COOKIE_NAME, token, { httpOnly: true, secure: true });
+  setCookie(c, AUTH_COOKIE_NAME, token, { httpOnly: true, secure: true, expires: new Date(Date.now() + 60 * 60 * 24 * 365 * 1000) });
 }
 
 export function logout(c: ContextType) {
